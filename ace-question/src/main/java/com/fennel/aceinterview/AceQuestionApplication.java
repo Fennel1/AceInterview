ac24eb7f0253fcc24dcf20a2f162876313f6d519
Fennel1 <627235787@qq.com>
2025-05-29 22:10:20 +0800

    renren & mybatisplus

--- ace-question/src/main/java/com/fennel/aceinterview/AceQuestionApplication.java
+++ ace-question/src/main/java/com/fennel/aceinterview/AceQuestionApplication.java
@@ -1,8 +1,10 @@
-package com.fennel.aceintervirew.acequestion;
+package com.fennel.aceinterview.acequestion;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.fennel.aceinterview.question.dao")
 @SpringBootApplication
 public class AceQuestionApplication {
 
